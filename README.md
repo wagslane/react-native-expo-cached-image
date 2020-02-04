6ff24a02c35232d304bf8683ef8165e0ece97d8e
lane-c-wagner <lane.c.wagner@gmail.com>
2020-02-04 06:23:35 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # react-native-expo-cached-image
+
 Cached image component for Expo's managed workflow
+
+## Installation
+
+`expo install react-native-expo-cached-image`
+
+or
+
+`yarn add react-native-expo-cached-image`
+
+or
+
+`npm install react-native-expo-cached-image`
+
+## Quick Start
+
+```javascript
+import CachedImage from 'react-native-expo-cached-image';
+
+// In render()
+<CachedImage source={{ uri: 'https://qvault.io/wp-content/uploads/2019/05/QVault-app.png' }}/>
+
+```
+
+The component will download the image to the user's local filesystem using a deterministic hash
+of the URI as the path key. If the image is already downloaded, it will be rendered without re-downloading.
+
+### Props
+
+CachedImage is a direct wrapper of [react-native Image](https://facebook.github.io/react-native/docs/image). As such, all of its props are available as props to CachedImage. Styles are also passed down.
+
+#### ImageBackground
+
+CachedImage can optionally be used as a wrapper of [react-native ImageBackground](https://facebook.github.io/react-native/docs/imagebackground). To do so, pass in the prop isBackground={true}.
+
+```javascript
+import CachedImage from 'react-native-expo-cached-image';
+
+// In render()
+<CachedImage isBackground source={{ uri: 'https://qvault.io/wp-content/uploads/2019/05/QVault-app.png' }}/>
+
+```
+
+## Contributing
+
+Feel free to contribute! This package is open-source, just make sure that your pull request passes linting and tests:
+
+```bash
+yarn lint
+```
+
+```bash
+yarn test
+```
---
+++ index.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { View, Image, ImageBackground } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as Crypto from 'expo-crypto';
+
+export default class CachedImage extends Component {
+  state = {
+    imgURI: ''
+  }
+
+  async componentDidMount() {
+    const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
+    await this.loadImage(filesystemURI, this.props.source.uri);
+  }
+
+  async componentDidUpdate() {
+    const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
+    if (this.props.source.uri === this.state.imgURI ||
+      filesystemURI === this.state.imgURI) {
+      return null;
+    }
+    await this.loadImage(filesystemURI, this.props.source.uri);
+  }
+
+  async getImageFilesystemKey(remoteURI) {
+    const hashed = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      remoteURI
+    );
+    return `${FileSystem.cacheDirectory}${hashed}`;
+  }
+
+  async loadImage(filesystemURI, remoteURI) {
+    try {
+      // Use the cached image if it exists
+      const metadata = await FileSystem.getInfoAsync(filesystemURI);
+      if (metadata.exists) {
+        this.setState({
+          imgURI: filesystemURI
+        });
+        return;
+      }
+
+      // otherwise download to cache
+      const imageObject = await FileSystem.downloadAsync(
+        remoteURI,
+        filesystemURI
+      );
+      this.setState({
+        imgURI: imageObject.uri
+      });
+    }
+    catch (err) {
+      console.log('Image loading error:', err);
+      this.setState({ imgURI: remoteURI });
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        {this.props.isBackground ? (
+          <ImageBackground
+            {...this.props}
+            source={this.state.imgURI ? { uri: this.state.imgURI } : null}
+          >
+            {this.props.children}
+          </ImageBackground>
+        ) : (
+          <Image
+            {...this.props}
+            source={this.state.imgURI ? { uri: this.state.imgURI } : null}
+          />
+        )}
+      </View>
+    );
+  }
+}
