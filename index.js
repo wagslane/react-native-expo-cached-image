177cf319d8912ba6a65342e10e2fa79e2cbb44e2
Sharath <sharathnasa@gmail.com>
2020-05-02 12:05:33 +0530

    Revert "1. Fixed the issue of image/imagebackground not displaying due to not supplying the style as the prop 2. Added the React-Native ActivityIndicator while downling and caching the image as well as while fetching the image from the cache. 3. Added the support to pass style as the props for the above points."
    This reverts commit 9f72bc84

--- index.js
+++ index.js
@@ -1,92 +1,78 @@
-import React, {Component} from 'react';
-import {View, Image, ImageBackground, ActivityIndicator} from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, ImageBackground } from 'react-native';
 import * as FileSystem from 'expo-file-system';
 import * as Crypto from 'expo-crypto';
 
 export default class CachedImage extends Component {
-    state = {
-        imgURI: '',
-        loading: true,
+  state = {
+    imgURI: ''
+  }
+
+  async componentDidMount() {
+    const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
+    await this.loadImage(filesystemURI, this.props.source.uri);
+  }
+
+  async componentDidUpdate() {
+    const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
+    if (this.props.source.uri === this.state.imgURI ||
+      filesystemURI === this.state.imgURI) {
+      return null;
     }
+    await this.loadImage(filesystemURI, this.props.source.uri);
+  }
 
-    async componentDidMount() {
-        const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
-        await this.loadImage(filesystemURI, this.props.source.uri);
+  async getImageFilesystemKey(remoteURI) {
+    const hashed = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      remoteURI
+    );
+    return `${FileSystem.cacheDirectory}${hashed}`;
+  }
+
+  async loadImage(filesystemURI, remoteURI) {
+    try {
+      // Use the cached image if it exists
+      const metadata = await FileSystem.getInfoAsync(filesystemURI);
+      if (metadata.exists) {
+        this.setState({
+          imgURI: filesystemURI
+        });
+        return;
+      }
+
+      // otherwise download to cache
+      const imageObject = await FileSystem.downloadAsync(
+        remoteURI,
+        filesystemURI
+      );
+      this.setState({
+        imgURI: imageObject.uri
+      });
     }
-
-    async componentDidUpdate() {
-        const filesystemURI = await this.getImageFilesystemKey(this.props.source.uri);
-        if (this.props.source.uri === this.state.imgURI ||
-            filesystemURI === this.state.imgURI) {
-            return null;
-        }
-        await this.loadImage(filesystemURI, this.props.source.uri);
+    catch (err) {
+      console.log('Image loading error:', err);
+      this.setState({ imgURI: remoteURI });
     }
+  }
 
-    async getImageFilesystemKey(remoteURI) {
-        const hashed = await Crypto.digestStringAsync(
-            Crypto.CryptoDigestAlgorithm.SHA256,
-            remoteURI
-        );
-        return `${FileSystem.cacheDirectory}${hashed}`;
-    }
-
-    async loadImage(filesystemURI, remoteURI) {
-        try {
-            // Use the cached image if it exists
-            const metadata = await FileSystem.getInfoAsync(filesystemURI);
-            if (metadata.exists) {
-                this.setState({
-                    imgURI: filesystemURI,
-                    loading: false
-                });
-                return;
-            }
-
-            // otherwise download to cache
-            const imageObject = await FileSystem.downloadAsync(
-                remoteURI,
-                filesystemURI
-            );
-            this.setState({
-                imgURI: imageObject.uri,
-                loading: false
-            });
-        } catch (err) {
-            console.log('Image loading error:', err);
-            this.setState({imgURI: remoteURI});
-        }
-    }
-
-    render() {
-        if (this.state.loading && this.props.showActivityIndicator) {
-            // while the image is being checked and downloading
-            return (
-                <View style={this.props.activityIndicatorStyle}>
-                    <ActivityIndicator
-                        color={this.props.activityIndicatorColor ? this.props.activityIndicatorColor : '#42C2F3'}
-                        size={this.props.activityIndicatorSize ? this.props.activityIndicatorSize : 'small'}
-                    />
-                </View>
-            );
-        }
-
-        return (
-            <View>
-                {this.props.isBackground ? (
-                    <ImageBackground style={this.props.cachedImageStyles}
-                                     {...this.props}
-                                     source={this.state.imgURI ? {uri: this.state.imgURI} : null}
-                    >
-                        {this.props.children}
-                    </ImageBackground>
-                ) : (
-                    <Image style={this.props.cachedImageStyles}
-                           {...this.props}
-                           source={this.state.imgURI ? {uri: this.state.imgURI} : null}
-                    />
-                )}
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        {this.props.isBackground ? (
+          <ImageBackground
+            {...this.props}
+            source={this.state.imgURI ? { uri: this.state.imgURI } : null}
+          >
+            {this.props.children}
+          </ImageBackground>
+        ) : (
+          <Image
+            {...this.props}
+            source={this.state.imgURI ? { uri: this.state.imgURI } : null}
+          />
+        )}
+      </View>
+    );
+  }
 }
